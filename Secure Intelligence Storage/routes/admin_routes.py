ba3f7c268e62c56c3c0a8ce8a99452f131287623
Abdul Nafey <abdul.nafey18education@gmail.com>
2025-03-20 13:51:01 +0000

    Fixed log monitoring issue

--- Secure Intelligence Storage/routes/admin_routes.py
+++ Secure Intelligence Storage/routes/admin_routes.py
@@ -4,6 +4,11 @@ from security.nmap_scanner import scan_network
 from database.mongo_db import db  # Import MongoDB connection
 
 def admin_routes(app):
+    @app.route('/get_logs/<log_type>', methods=['GET'])
+    def get_logs(log_type):
+        logs = list(db.logs.find({"type": log_type}, {"_id": 0}))  # Fetch logs of selected type
+        return jsonify(logs)
+    
     @app.route('/scan_network', methods=['POST'])
     def trigger_scan():
         print("[INFO] Scan request received!")
